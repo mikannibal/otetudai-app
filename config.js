f71536892ecbafcf5c16efa73eff7eff6c2fd708
mikannibal <kanomika@gmail.com>
2026-05-29 16:26:53 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.OTEDUTAI_CONFIG = {
-  SUPABASE_URL: "",
-  SUPABASE_ANON_KEY: "",
+  SUPABASE_URL: "https://waarmxldwmnmqsgfwuoy.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_RFPoEuhyOctd5vwTGTCBLw_euO0jGC6",
   ENABLE_REALTIME: true,
 };
